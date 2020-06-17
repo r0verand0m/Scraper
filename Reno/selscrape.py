7fd37908fca3da44bde09ba4ff190d53baee8b95
r0verand0m <alex.iugan@rovah.ca>
2020-06-17 17:12:10 -0400

    few changes...

---
+++ Reno/selscrape.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path = "c:\Users\alexi\Desktop\Scraper\Reno\chromedriver.exe")
